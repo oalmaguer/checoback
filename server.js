4a0fb65fb583ccf6e4839f1129a1b0cb19d38bc5
Oliver <almaguero95@gmail.com>
2024-05-27 19:40:52 -0700

    added server

---
+++ server.js
@@ -0,0 +1,20 @@
+// Step 1: Import the parts of the module you want to use
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/webhook", (req, res) => {
+  console.log("webhjookkkkk");
+  res.send("Webhook found");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
